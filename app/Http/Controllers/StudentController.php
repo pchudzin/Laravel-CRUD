6f80d90bb510424b999a4f0076854ed71bf98a84
Paweł Chudziński <39808870+pchudzin@users.noreply.github.com>
2024-04-04 14:02:15 +0200

    first

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,21 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\Student;
+use Illuminate\View\View;
 
 class StudentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $students = Student::all();
+        return view ('students.index')->with('students', $students);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         //
     }
@@ -33,7 +38,7 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): View
     {
         //
     }
@@ -41,7 +46,7 @@ class StudentController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
         //
     }
@@ -49,7 +54,7 @@ class StudentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
         //
     }
@@ -57,7 +62,7 @@ class StudentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
         //
     }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $table = 'contacts';
+    protected $table = 'students';
     protected $primaryKey = 'id';
     protected $fillable = ['name', 'address', 'mobile'];
 }
